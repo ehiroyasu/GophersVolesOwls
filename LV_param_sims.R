3d9a07407f1ac11642c5ba82281249d1302fae4a
ehiroyasu <ehiroyasu@bren.ucsb.edu>
2016-08-05 11:54:31 -0700

    parameter simulations to examine parameter sensitivities

---
+++ LV_param_sims.R
@@ -0,0 +1,32 @@
+parameters <- c(r = log(3.47), alpha = 0.3, beta = 0.5, delta = 0.5, K_prey = 175, K_pred = 6)
+state<-c(N = 10, P = 1)
+times<- seq(0, 100, by = 0.01)
+
+pred_prey <- function(t, state, parameters){
+  with(as.list(c(state, parameters)), {
+    #rate of change
+    #dprey=N*r - alpha * P * N #without density dependence, remember to remove K from vector of parameters
+    dprey= N * (r-alpha*P)*(1-(N/K_prey)) #with density dependence in prey
+    dpredator = P*(beta * N * alpha - delta)*(1-(P/K_pred))#with density dependence in predator
+    
+    #return rate of change
+    list(c(dprey, dpredator))
+  })
+  
+}
+
+out<- ode(y=state, times = times, func = pred_prey, parms = parameters)
+matplot(out, xlab="time", main = "Predator Prey Model")
+
+
+#looping through three different alpha values, the writing to a list
+alpha<-c(0.03, 0.3, 3.0)
+
+out_alpha_sim<-vector("list", 3)
+names(out_alpha_sim)=paste("alpha=", alpha)
+
+for(i in 1:3){
+  out_alpha_sim[[i]]<- ode(y=state, times = times, func = pred_prey, parms = c(r = log(3.47), alpha=alpha[i], beta = 0.5, delta = 0.5, K_prey = 175, K_pred = 6))
+}
+
+matplot
