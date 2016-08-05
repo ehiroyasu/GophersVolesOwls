e3690a4d7d9124ca5306ce56cf21a0b94f8a629e
ehiroyasu <ehiroyasu@bren.ucsb.edu>
2016-08-05 11:25:56 -0700

    added carrying capacity for both owls and gophers. Adjusted the parameters to be more clear

--- LotkaVolteraPP.R
+++ LotkaVolteraPP.R
@@ -7,12 +7,13 @@ library(deSolve)
 #N = starting population of prey
 #P = starting population of predator
 #K = carrying capacity of prey
-#alpha = attack rate of predator
-#epsilon= assimilation efficiency (efficiency of turning gophers into chicks)
+#alpha = attack rate of predator (or capture efficiency; the larger alpha is, the more the per
+  #capita growth rate of the prey population is depressed by the addition of a single predator)
+#beta= assimilation efficiency (efficiency of turning gophers into per capita growth)
 #delta = death rate of predator
 
 #vectors of parameters and state variables, this allows us to easily change either of them
-parameters <- c(r = log(3.47), alpha = 0.5, epsilon = 0.5, delta = 0.5, K = 15)
+parameters <- c(r = log(3.47), alpha = 0.3, beta = 0.5, delta = 0.5, K_prey = 150, K_pred = 6)
 state<-c(N = 10, P = 1)
 times<- seq(0, 100, by = 0.01)
 
@@ -20,8 +21,8 @@ pred_prey <- function(t, state, parameters){
   with(as.list(c(state, parameters)), {
     #rate of change
     #dprey=N*r - alpha * P * N #without density dependence, remember to remove K from vector of parameters
-    dprey= N * (r * (1-(N/K)) - (alpha * P)) #with density dependence in prey
-    dpredator = epsilon * P * N * alpha - delta * P
+    dprey= N * (r-alpha*P)*(1-(N/K_prey)) #with density dependence in prey
+    dpredator = P*(beta * N * alpha - delta)*(1-(P/K_pred))#with density dependence in predator
     
     #return rate of change
     list(c(dprey, dpredator))
