d3ac33d284d8bccb87373e54776289afaac5c11d
ehiroyasu <ehiroyasu@bren.ucsb.edu>
2016-08-09 21:22:39 -0700

    sensitivity analysis for parameters

--- LV_param_sims.R
+++ LV_param_sims.R
@@ -1,6 +1,6 @@
 parameters <- c(r = log(3.47), alpha = 0.3, beta = 0.5, delta = 0.5, K_prey = 175, K_pred = 6)
 state<-c(N = 10, P = 1)
-times<- seq(0, 100, by = 0.01)
+times<- seq(0, 10, by = 1)
 
 pred_prey <- function(t, state, parameters){
   with(as.list(c(state, parameters)), {
@@ -20,13 +20,17 @@ matplot(out, xlab="time", main = "Predator Prey Model")
 
 
 #looping through three different alpha values, the writing to a list
-alpha<-c(0.03, 0.3, 3.0)
+alpha<-c(0.001, 0.01, 0.1)
 
-out_alpha_sim<-vector("list", 3)
-names(out_alpha_sim)=paste("alpha=", alpha)
+out_alpha_sim<-vector("list", length(alpha))
+names(out_alpha_sim)=alpha
 
-for(i in 1:3){
+for(i in 1:length(alpha)){
   out_alpha_sim[[i]]<- ode(y=state, times = times, func = pred_prey, parms = c(r = log(3.47), alpha=alpha[i], beta = 0.5, delta = 0.5, K_prey = 175, K_pred = 6))
 }
 
-matplot
+#Plotting different outcomes
+matplot(out_alpha_sim[[1]], lty=1:2, type='l', col=1)
+matplot(out_alpha_sim[[2]], add=TRUE,lty = 1:2, type = 'l', col=3)
+matplot(out_alpha_sim[[3]], add=TRUE, lty=1:2, type='l', col = 5)
+
