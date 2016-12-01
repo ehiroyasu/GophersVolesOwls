139a593250a8ffceebccf2ac35069f507895fe2d
Elizabeth <ehiroyasu@bren.ucsb.edu>
2016-12-01 09:30:40 -0800

    fixed subset function to be used in plot.

--- owls/R/plot_n.R
+++ owls/R/plot_n.R
@@ -3,19 +3,18 @@
 #'Uses ggplot2 package to plot the output data from predprey_sim. Data must be processed into a dataframe using the lsplit
 #'and df_sim functions.
 #'
-#'@param df
-#'@param r is the r values specified in the parameters 
+#'@param df is the dataframe to feed into ggplot2, it is the output from the df_sim function in the 'owls' package.
+#'@param r is the r values specified in the parameters, because this function subsets by r value, it must be specified 
+#'in both the r param and the ssubset parameter.
+#'@param ssubset is a character expression (requires nested quotes) to specify the r value that the data is subsetted by.
 #'
 #'@author Elizabeth Hiroyasu
 #'
 #'
 
 
-
-#subset function doesn't work...
-
-plot_n<-function(df, r){
-  ggplot(data=subset(df, r==0.01), aes(x=time, y=N, group=as.factor(P), color=as.factor(P))) + 
+plot_n<-function(df, ssubset, r){
+  ggplot(data=subset(df, eval(parse(text=ssubset))), aes(x=time, y=N, group=as.factor(P), color=as.factor(P))) + 
     geom_line(size=1.5)+ ggtitle(paste("r=", r)) + scale_colour_discrete(name="Predator Density") + 
     theme(legend.background=element_rect(fill="gray90", size=0.5, linetype="solid", colour=1))
 }
