d03f9564f36d45fc48d00465aae665bba9eda566
Elizabeth <ehiroyasu@bren.ucsb.edu>
2016-12-06 12:17:03 -0800

    edited column renaming

--- owls/R/df_sim.R
+++ owls/R/df_sim.R
@@ -17,11 +17,9 @@ df_sim<-function(sim, r){
     foo[[i]]<-apply(sim[[i]], 3, data.frame)
     foo[[i]]<-ldply(foo[[i]], data.frame)
     foo[[i]]$r<-r[i]
+    foo[[i]]<-rename(foo[[i]], c("X1"="time", "X2"="N", "X3"="P"))
   }
-  foo<-rbind.fill(foo)
-  foo<-rename(foo, c("X1"="time", "X2"="N", "X3"="P"))
-  
-  return(foo)
+    return(foo)
 }
 
 
