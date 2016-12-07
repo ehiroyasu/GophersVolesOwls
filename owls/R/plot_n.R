7db079b6ca99beaecdbaa421c2716afc9b7640bb
Elizabeth <ehiroyasu@bren.ucsb.edu>
2016-12-07 13:52:09 -0800

    removed ylim from function.

--- owls/R/plot_n.R
+++ owls/R/plot_n.R
@@ -4,8 +4,7 @@
 #'and df_sim functions.
 #'
 #'@param foo is the dataframe to feed into ggplot2, it is the subsetted dataframe from the df_sim function in the 'owls' package.
-#'@param r is the r values specified in the parameters, because this function subsets by r value, it must be specified 
-#'in both the r param and the ssubset parameter.
+#'@param r is the r values specified in the parameters, because this function subsets by r value.
 #'
 #'@author Elizabeth Hiroyasu
 #'
@@ -15,9 +14,9 @@
 plot_n<-function(df, r){
   
   ggplot(data=df, aes(x=time, y=N, group=as.factor(P), color=as.factor(P))) + 
-    geom_line(size=1.5)+ ggtitle(paste("r=", r)) + theme(plot.title=element_text(size=12)) + 
+    geom_line(size=1.1)+ ggtitle(paste("r=", r)) + theme(plot.title=element_text(size=12)) + 
     theme(legend.background=element_rect(fill="gray90", size=0.5, linetype="solid", colour=1),  
-          legend.direction="vertical")+scale_colour_discrete(name="Predator Density") + ylim(25, 175)
+          legend.direction="vertical")+scale_colour_discrete(name="Predator Density")  
 }
 
 
