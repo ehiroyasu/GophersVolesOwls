508ff195f78eb4f36b2e104cd97fbeb077f8651b
Elizabeth <ehiroyasu@bren.ucsb.edu>
2017-04-06 12:05:49 -0700

    new function to create percent difference plots

---
+++ owls/R/percent_plot.R
@@ -0,0 +1,37 @@
+#'Plot percent difference
+#'
+#'Plots the percent change in prey by different levels of predator density
+#' 
+#'@param sim is the output list from the function n.sim (which is modified from the predprey_sim output) 
+#'@param r is a vector of population growth rates
+#'
+#'@author Elizabeth Hiroyasu
+#'
+
+
+percent_plot<-function(sim, r){
+  diff.n.init<-NULL
+  for(i in 1:length(sim)){
+    diff.n.init[[i]]<-percent_diff(sim[[i]], r)
+  }
+
+  p<-NULL 
+  for(i in 1:length(diff.n.init)){
+    p[[i]]<-plot_diff(diff=diff.n.init[[i]], N=N, j=i)
+  }
+
+  legend<-get_legend(p[[1]])
+  
+  grid.arrange(grobs=list(p[[1]]+theme(legend.position="none"), 
+                          p[[2]]+theme(legend.position="none"), 
+                          p[[3]]+theme(legend.position="none"), 
+                          p[[4]]+theme(legend.position="none"),
+                          p[[5]]+theme(legend.position="none"),
+                          p[[6]]+theme(legend.position="none"),
+                          legend), 
+               nrow=4, top=textGrob("Percent Change by Predator Density", gp=gpar(fontface="bold", fontsize=6, cex=3)),
+               layout_matrix = rbind(c(1,2), c(3,4), c(5,6),c(7,7)), heights = c(4, 4, 4, 0.4))
+}
+
+
+
