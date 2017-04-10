aff36f33984e2d93d6a6f3e4929da80ef80405d0
ehiroyasu <ehiroyasu@bren.ucsb.edu>
2017-04-10 12:25:08 -0700

    adjusted plotting function to adjust axes labels

--- owls/R/percent_plot.R
+++ owls/R/percent_plot.R
@@ -22,15 +22,17 @@ percent_plot<-function(sim, r){
 
   legend<-get_legend(p[[1]])
   
-  grid.arrange(grobs=list(p[[1]]+theme(legend.position="none"), 
-                          p[[2]]+theme(legend.position="none"), 
-                          p[[3]]+theme(legend.position="none"), 
-                          p[[4]]+theme(legend.position="none"),
-                          p[[5]]+theme(legend.position="none"),
-                          p[[6]]+theme(legend.position="none"),
+  grid.arrange(grobs=list(p[[1]]+theme(legend.position="none", axis.title.y=element_blank()), 
+                          p[[2]]+theme(legend.position="none", axis.title.y=element_blank()), 
+                          p[[3]]+theme(legend.position="none", axis.title.y=element_blank()), 
+                          p[[4]]+theme(legend.position="none", axis.title.y=element_blank()),
+                          p[[5]]+theme(legend.position="none", axis.title.y=element_blank()),
+                          p[[6]]+theme(legend.position="none", axis.title.y=element_blank()),
                           legend), 
                nrow=4, top=textGrob("Percent Change by Predator Density", gp=gpar(fontface="bold", fontsize=6, cex=3)),
-               layout_matrix = rbind(c(1,2), c(3,4), c(5,6),c(7,7)), heights = c(4, 4, 4, 0.4))
+               left=textGrob("Percent Difference", rot=90, vjust=1),
+               layout_matrix = rbind(c(1,2), c(3,4), c(5,6), c(7,7)), heights = c(2, 2, 2, 0.3),
+               clip=FALSE)
 }
 
 
