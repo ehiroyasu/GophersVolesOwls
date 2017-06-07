aa08253b7e989f66ed26b8fee0c47ce19e3608bb
Elizabeth <ehiroyasu@bren.ucsb.edu>
2017-06-07 12:17:01 -0700

    added code to differentiate between N1 and N2 plots. Need to adjust legends.

--- owls/R/plot_eq.R
+++ owls/R/plot_eq.R
@@ -1,4 +1,4 @@
-#'plot_eq
+D#'plot_eq
 #'
 #'Uses ggplot2 package to plot the output data from predprey_sim. Data must be processed into a dataframe using the lsplit
 #'and df_sim functions.
@@ -9,14 +9,17 @@
 #'
 #'
 
-plot_eq<-function(df){
-  ggplot(data=df, aes(x=D, y=Eq, group=as.factor(K_prey), color=as.factor(K_prey))) +
-    geom_line(size=1.1)+
-    labs(title=paste("Equilibrium prey density, Predator Density=", P), x="D (Individuals/ha)",
-                             y="Equilibrium Prey Density (Individuals/ha")+
+plot_eq<-function(N1, N2, P, i){
+  ggplot(mapping=aes(D, Eq, group=as.factor(K_prey), color=as.factor(K_prey)))+
+    geom_line(data=N1, size=1.1, linetype=1)+
+    geom_line(data=N2, size=1.1, linetype=2)+
+    labs(title=paste("Equilibrium prey density, Predator Density=", P[i]), x="D (Individuals/ha)",
+         y="Equilibrium Prey Density (Individuals/ha")+
     theme(plot.title=element_text(size=12))+
-    theme(legend.background=element_rect(fill="gray90", size=0.5, linetype="solid", colour=1), 
-          legend.direction="vertical") + 
+    #scale_linetype_identity(name="Equilibria", guide=guide_legend(override.aes = list(linetype=c(1,2))),
+    #                        labels=c("N1", "N2"), breaks=c("solid","dashed"))+
     scale_colour_discrete(name="Prey Carrying Capacity")+
+    theme(legend.background=element_rect(fill="gray90", size=0.5, linetype="solid", colour=1), 
+          legend.direction="vertical") +
     facet_grid(.~r)
 }
