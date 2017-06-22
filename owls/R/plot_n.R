6c766b96ff5cbea83081d33f679de6d96cab6b95
Elizabeth <ehiroyasu@bren.ucsb.edu>
2017-06-22 10:35:30 -0700

    generalized the plot_n function so plot_n_grid function is no longer necessary

--- owls/R/plot_n.R
+++ owls/R/plot_n.R
@@ -5,18 +5,22 @@
 #'
 #'@param df is the dataframe to feed into ggplot2, it is the subsetted dataframe from the df_sim function in the 'owls' package.
 #'@param r is the r values specified in the parameters, because this function subsets by r value.
+#'@param N is a vector of initial prey density values, in this function it is used in the title to indicate the starting value plotted
+#'@param K_prey is the carrying capacity of the prey population.
+#'@param i is the iterator used to cycle through the initial N values to be pasted into the title.
 #'
 #'@author Elizabeth Hiroyasu
 #'
 #'
 
 
-plot_n<-function(df, r){
+plot_n<-function(df, r, N, K_prey, i){
   
   ggplot(data=df, aes(x=time, y=N, group=as.factor(P), color=as.factor(P))) + 
-    geom_line(size=1.1)+ ggtitle(paste("r=", r)) + theme(plot.title=element_text(size=12)) + 
+    geom_line(size=1.1)+ ggtitle(paste("N=", unique(N)[i], ", K=", K_prey)) + theme(plot.title=element_text(size=12)) + 
     theme(legend.background=element_rect(fill="gray90", size=0.5, linetype="solid", colour=1),  
-          legend.direction="vertical")+scale_colour_discrete(name="Predator Density")  
+          legend.direction="vertical")+ scale_colour_discrete(name="Predator Density") +
+    facet_grid(.~r) 
 }
 
 
