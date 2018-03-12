fb19ae871a82ada7ac3c1ba4c6cd9f707b464e7c
Elizabeth <ehiroyasu@bren.ucsb.edu>
2018-03-12 15:46:07 -0700

    used to create alpha_param_exp file

--- Summer 2016 Work/LV_param_sims.R
+++ Summer 2016 Work/LV_param_sims.R
@@ -1,5 +1,5 @@
-parameters <- c(r = log(3.47), alpha = 0.3, beta = 0.5, delta = 0.5, K_prey = 175, K_pred = 6)
-state<-c(N = 10, P = 1)
+#parameters <- c(r = log(3.47), alpha = 0.3, beta = 0.5, delta = 0.5, K_prey = 175, K_pred = 6)
+state<-c(N = 500, P = 1)
 times<- seq(0, 10, by = 1)
 
 pred_prey <- function(t, state, parameters){
@@ -7,27 +7,30 @@ pred_prey <- function(t, state, parameters){
     #rate of change
     #dprey=N*r - alpha * P * N #without density dependence, remember to remove K from vector of parameters
     dprey= N * (r-alpha*P)*(1-(N/K_prey)) #with density dependence in prey
-    dpredator = P*(beta * N * alpha - delta)#*(1-(P/K_pred))#with density dependence in predator
-    
+    #dpredator = P*(beta * N * alpha - delta)#*(1-(P/K_pred))#with density dependence in predator
+    dpredator=0
     #return rate of change
     list(c(dprey, dpredator))
   })
   
 }
 
-out<- ode(y=state, times = times, func = pred_prey, parms = parameters)
-matplot(out, xlab="time", main = "Predator Prey Model")
+#out<- ode(y=state, times = times, func = pred_prey, parms = parameters)
+#matplot(out[,2:3], xlab="time", main = "Predator Prey Model", type="l")
 
 
 #looping through three different alpha values, the writing to a list
-alpha<-c(0.001, 0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3)
+alpha<-c(0.001, 0.01, 0.1, 0.25, 1.0, 1.5, 2)
 
 out_alpha_sim<-vector("list", length(alpha))
 names(out_alpha_sim)=alpha
 
 for(i in 1:length(alpha)){
-  out_alpha_sim[[i]]<- ode(y=state, times = times, func = pred_prey, parms = c(r = log(3.47), alpha=alpha[i], beta = 0.5, delta = 0.5, K_prey = 175, K_pred = 6))
+  out_alpha_sim[[i]]<- ode(y=state, times = times, func = pred_prey, parms = c(r = log(3.47), alpha=alpha[i], K_prey = 1000))
 }
+
+alpha_sim_N<-ldply(out_alpha_sim, data.frame)
+ggplot(data=alpha_sim_N[,-4], aes(x=time, y=N, group=as.factor(.id), color=as.factor(.id)))+geom_line()
 
 #Plotting different outcomes
 matplot(out_alpha_sim[[1]], lty=1:2, type='l', col=1, main="Predator prey dynamics over time")
